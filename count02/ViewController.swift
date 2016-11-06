09d18593e3f63635399a359958c3f15c045518b0
前田直己 <shinki030201@gmail.com>
2016-11-06 16:23:43 +0900

    first commit

---
+++ count02/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  Count
+//
+//  Created by 前田直己 on 2015/07/31.
+//  Copyright (c) 2015年 前田直己. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var number: Int = 0
+    @IBOutlet var label: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func plus() {
+        number = number + 1
+        label.text = String(number)
+        
+                
+    }
+    
+       
+}
